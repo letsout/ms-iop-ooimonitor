4e5cd4ff042e3318e25ef7355dc581f802cc4d78
yx <739927980@qq.com>
2019-10-10 13:02:39 +0800

    注释多余代码

--- src/main/java/com/asiainfo/msooimonitor/mapper/dbt/ooi/InterfaceInfoMpper.java
+++ src/main/java/com/asiainfo/msooimonitor/mapper/dbt/ooi/InterfaceInfoMpper.java
@@ -13,43 +13,43 @@ import java.util.Map;
 
 @Repository
 public interface InterfaceInfoMpper {
-
-    /**
-     * 获取接口信息
-     *
-     * @param params
-     * @return
-     */
-    List<InterfaceInfo> getInterfaceInfo(Map params);
-
-    int checkInterface(String interfaceId);
-
-    void saveInterfceInfo(InterfaceInfo params);
-
-    /*   List<InterfaceRecord> getInterfaceRecord(@Param("start") int start,@Param("end") int end);*/
-
-    List<InterfaceRecord> getInterfaceRecord(Map params);
-
-    void saveInterfaceRecord(Map params);
-
-    void deleteInterfaceId(String interfaceId);
-
-    void editInterfaceInfo(InterfaceInfo params);
-
-    List<String> getDownloadFile(String date);
-
-    List<InterfaceInfo> getuploadInterface();
-
-    List<InterfaceInfo> getLoadInterface();
-
-    List<InterfaceRecord> getInterfaceRecordByParam(@Param("start") int start,
-                                                    @Param("end") int end,
-                                                    @Param("interfaceId") String interfaceId,
-                                                    @Param("updateType") String updateType,
-                                                    @Param("state") String state,
-                                                    @Param("updateTime") String updateTime);
-
-    String getInterfaceIdType(@Param("interfaceId") String interfaceId);
+//
+//    /**
+//     * 获取接口信息
+//     *
+//     * @param params
+//     * @return
+//     */
+//    List<InterfaceInfo> getInterfaceInfo(Map params);
+//
+//    int checkInterface(String interfaceId);
+//
+//    void saveInterfceInfo(InterfaceInfo params);
+//
+//    /*   List<InterfaceRecord> getInterfaceRecord(@Param("start") int start,@Param("end") int end);*/
+//
+//    List<InterfaceRecord> getInterfaceRecord(Map params);
+//
+//    void saveInterfaceRecord(Map params);
+//
+//    void deleteInterfaceId(String interfaceId);
+//
+//    void editInterfaceInfo(InterfaceInfo params);
+//
+//    List<String> getDownloadFile(String date);
+//
+//    List<InterfaceInfo> getuploadInterface();
+//
+//    List<InterfaceInfo> getLoadInterface();
+//
+//    List<InterfaceRecord> getInterfaceRecordByParam(@Param("start") int start,
+//                                                    @Param("end") int end,
+//                                                    @Param("interfaceId") String interfaceId,
+//                                                    @Param("updateType") String updateType,
+//                                                    @Param("state") String state,
+//                                                    @Param("updateTime") String updateTime);
+//
+//    String getInterfaceIdType(@Param("interfaceId") String interfaceId);
 
     List<Map<String, String>> getDetailEffect(@Param("activityIds") String activityIds, @Param("date") String date, @Param("start") int start, @Param("limit") int limit);
 
